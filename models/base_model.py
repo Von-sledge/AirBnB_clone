96c8bd79d37689410703611c06242d4a9b184653
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-06 01:13:08 -0800

    Base instance model for initialization

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,10 @@
    for other classes of AirBnB clone
 """
 
+from uuid import uuid4
+import datetime
+import models
+
 class BaseModel:
     """BaseModel Module that defines all common attributes/methods
        for other classes of AirBnB clone
@@ -16,4 +20,27 @@ class BaseModel:
         updated_at: datetime - assign with the current datetime when instance
         is created and it will be updated every time you change your object
     """
+    
+    def__init__(self):
+        """Initialization of BaseModel with unique ID"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datettime.today()
 
+    def __str__(self):
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
+        cls_name = self.__class__.__name__
+        return "[{}] [{}] [{}]".format(cls_name, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance update_at with the current datetime"""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+        cls = self.__dict__.copy()
+        cls["__class__"] = self.__class__.__name__
+        cls["created_at"] = self.created_at.isoformat()
+        cls["updated_at"] = self.updated_at.isoformat()
+        return cls
