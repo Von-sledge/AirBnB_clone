8e367a90bae394e6b24bd41617ab81c6f90d9804
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-06 00:22:22 -0800

    AirBnB clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,19 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""BaseModel Module that defines all common attributes/methods
+   for other classes of AirBnB clone
+"""
+
+class BaseModel:
+    """BaseModel Module that defines all common attributes/methods
+       for other classes of AirBnB clone
+
+       Public instance attributes:
+        id: string - assign with an uuid when an instance is created
+        
+        created_at: datetime - assign with the current datetime
+        when an instance is created
+        
+        updated_at: datetime - assign with the current datetime when instance
+        is created and it will be updated every time you change your object
+    """
+
