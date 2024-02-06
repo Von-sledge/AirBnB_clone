00f4bb5e251d3813117660ca0ffcd47941f0374d
Von-sledge <papaamoah568@gmail.com>
2024-02-06 10:47:28 +0000

    AirBnB clone

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,42 @@
 #!/usr/bin/python3
-"""BaseModel Module that defines all common attributes/methods
-   for other classes of AirBnB clone
-"""
+"""BaseModel Module defines all common attributes/methods for classes of AirBnB clone."""
 
 from uuid import uuid4
 import datetime
-import models
+#import models
+
 
 class BaseModel:
-    """BaseModel Module that defines all common attributes/methods
-       for other classes of AirBnB clone
-
-       Public instance attributes:
-        id: string - assign with an uuid when an instance is created
-        
-        created_at: datetime - assign with the current datetime
-        when an instance is created
-        
-        updated_at: datetime - assign with the current datetime when instance
-        is created and it will be updated every time you change your object
     """
-    
-    def__init__(self):
-        """Initialization of BaseModel with unique ID"""
+    BaseModel Module that defines all common attributes/methods.
+
+    for other classes of AirBnB clone.
+    Public instance attributes:\
+    id: string - assign with an uuid when an instance is created.
+    created_at: datetime - assign with the current datetime\
+    when an instance is created.
+    updated_at: datetime - assign with the current datetime when instance.
+    is created and it will be updated every time you change your object.
+    """
+
+    def __init__(self):
+        """Initialize BaseModel with unique ID."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datettime.today()
 
     def __str__(self):
-        """should print: [<class name>] (<self.id>) <self.__dict__>"""
+        """Print: [<class name>] (<self.id>) <self.__dict__>."""
         cls_name = self.__class__.__name__
         return "[{}] [{}] [{}]".format(cls_name, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance update_at with the current datetime"""
+        """Update the public instance update_at with the current datetime."""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
+        """Return a dictionary containing all keys/values of __dict__."""
         cls = self.__dict__.copy()
         cls["__class__"] = self.__class__.__name__
         cls["created_at"] = self.created_at.isoformat()
