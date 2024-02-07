f1e56e8def9b089526beb16c76fd84ebc53d6d3b
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-07 04:06:50 -0800

    CLI Consile

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""This contains the entry point of the command interpreter"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """cli - command line interface class"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        "Exit command at End of File"
+        print("")
+        return True
+
+    def do_emptyline(self):
+        """Custom empty line + Enter does not execute anything"""
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
