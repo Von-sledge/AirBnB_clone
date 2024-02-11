61334189369008df39d708d861e4c791a60c6857
Von-sledge <papaamoah568@gmail.com>
2024-02-11 14:29:30 +0000

    AirBnB clone

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Module to test the instances of the amenities."""
+from tests.test_model.test_base_model import test_basemodel
+from model.amenity import Amenity
+
+
+class test_Amenity(test_basemodel):
+    """Class for Amenity and to test its instances."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the self method for the class."""
+        super().__init__(*args, **kwargs)
+        self.name = "Amenity"
+        self.value = Amenity
+
+    def test_name2(self):
+        """Test for any new value."""
+        new = self.value()
+        self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,1 +1,34 @@
 #!/usr/bin/python3
+"""Module to test the instances of Class test_user."""
+from tests.test_models.test_basemodel import test_basemodel
+from models.user import User
+
+
+
+class test_User(test_basemodel):
+    """Test various instances of user."""
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name - "User"
+        self.value = User
+
+    def test_first_name(self):
+        """Test for the first name."""
+        new = self.value()
+        self.assertEqual(type(new.first_name), str)
+
+    def test_last_name(self):
+        """Test for the last name."""
+        new = self.value()
+        self.assertEqual(type(new.last_name), str)
+
+    def test_email(self):
+        """Test for the e-mail."""
+        new = self.value()
+        self.assertEqual(type(new.email), str)
+
+    def test_password(self):
+        """Test for the password."""
+        new = self.value()
+        self.assertEqual(type(new.password), str)
