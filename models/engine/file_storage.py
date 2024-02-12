72edf95f7c829bed5ffa4ceb59720c4112591353
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-12 13:20:56 -0800

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,25 +31,25 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.is"""
         obj_name = obj.__class__.__name__ + "." + obj.id
-        self.__objects[obj_name] = obj
+        FileStorage.__objects[obj_name] = obj
 
     def save(self):
         """serializes __objects to the JSON file path"""
-        json_file = {}
-        for key, value in self.__objects.items():
+        json_file = FileStorage.__objects
+        for key, value in FileStorage.__objects.items():
             json_file[key] = value.to_dict()
-        with open(self.__file_path, mode="w", encoding="utf-8") as f:
-            json.dumps(json_file, f)
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
+            json.dump(json_file, f)
 
     def reload(self):
         """desrializes the JSON file to __objects if it exists"""
-        if path.exists(self.__file_path):
-            with open(self.__file_path, mode="r", encoding="utf-8") as f:
+        if path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as f:
                 json_file = json.loads(f.read())
             for key, value in json_file.items():
                 self.__objects[key] = eval(value['__class__'])(**value)
