56b419b6a8e0968ab51afd132ad68b9eadd7dd0b
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-12 13:25:53 -0800

    update

--- console.py
+++ console.py
@@ -11,6 +11,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from datetime import datetime
 
 
 class HBNBCommand(cmd.Cmd):
